decdfb74a429c06d09fae744c1132bdad691c5db
Yohsuke Fukai <ysk@yfukai.net>
2022-12-01 17:45:11 +0900

    doing things .

--- src/microtailor/_stitcher.py
+++ src/microtailor/_stitcher.py
@@ -34,7 +34,8 @@ class Stitcher(BaseModel, extra=Extra.forbid):
                images : NumArray, 
                tile_indices : Optional[IntArray] = None, 
                estimated_positions : Optional[NumArray] = None,
-               allowed_error : float = 10):
+               overlap_threshold : float = 5,
+               allowed_error : float = 20):
         """Calculate stitched positions of mosaic images.
 
         Parameters
@@ -45,17 +46,28 @@ class Stitcher(BaseModel, extra=Extra.forbid):
             The integer index of the tiles. If None, `estimated_positions` must be supplied.
         estimated_positions : Optional[NumArray], optional
             The estimated position of the tiles in pixel. If None, `tile_indices` must be supplied.
+        overlap_threshold : float, optional
+            The area percentage threshold to calculate pair displacement between tiles. Effective only when tile_indices is None.
         allowed_error : float, optional
-            The allowed error from the `estimated_positions` in pixel, by default 10.
+            The allowed error from the `estimated_positions` in pixel, by default 20.
         """
 
         if tile_indices is None and estimated_positions is None:
             raise ValueError("tile_indices and estimted_positions must not be None at the same time.")
 
         # construct the initial dataframe and 
-        image_pair_df = pd.DataFrame()
+
+        image_pairs = []
+        if tile_indices is not None:
+            for ind in tile_indices:
+                image_pairs.append({
+                    
+                })
+                
+        else:
+            
        
-        position_candidates = self.candidate_picker(images, coordinate_df["tile_pos"], coordinate_df[""])
+        position_candidates = self.candidate_picker(images, image_pair_indices)
         coordinate_df["position_candidate"] = position_candidates
 
         interpolated_positions = self.position_interpolator(images, coordinate_df)
